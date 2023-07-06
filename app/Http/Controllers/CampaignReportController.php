1f0aea59a5653154af6e78a927137a3279cd35bd
Nayaaa <inayahdwi07@gmail.com>
2023-07-06 22:16:04 +0700

    requirement for withdraw and report

--- app/Http/Controllers/CampaignReportController.php
+++ app/Http/Controllers/CampaignReportController.php
@@ -55,7 +55,11 @@ class CampaignReportController extends Controller
 
     public function store(Request $request)
     {
-        // create receipts
+        $id_campaign = $request->id_campaign;
+        $status = DB::table('campaigns')->where('id', $id_campaign)->value('status');
+
+        if ($status == "RUNNING") {
+            // create receipts
         $field_receipts = [];
         $receipts = Receipt::create($field_receipts);
 
@@ -83,6 +87,13 @@ class CampaignReportController extends Controller
             'data' => $data,
             'server_time' => (int) round(microtime(true) * 1000),
         ]);
+        } else{
+            return response()->json([
+                'status' => 'error',
+                'message' => 'The project has not started yet.'
+            ]);
+        }
+
     }
 
     public function show(Request $request, $id)
