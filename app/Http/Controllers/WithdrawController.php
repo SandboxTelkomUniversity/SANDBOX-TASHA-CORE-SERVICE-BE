3ac60f92a3af56d32be7c71f9e3796f957b42952
Nayaaa <inayahdwi07@gmail.com>
2023-07-15 22:36:07 +0700

    adjustment API detail withdraw

--- app/Http/Controllers/WithdrawController.php
+++ app/Http/Controllers/WithdrawController.php
@@ -119,5 +119,20 @@ class WithdrawController extends Controller
             'server_time' => (int) round(microtime(true) * 1000),
         ]);
     }
+    
+    public function detail_withdraw(Request $request, $id)
+    {
+        $data = DB::table('campaigns')
+            ->join('users', 'campaigns.id_user', '=', 'users.id')
+            ->join('user_banks', 'users.id_user_bank', '=', 'user_banks.id')
+            ->where('campaigns.id', $id)
+            ->select('campaigns.name', 'campaigns.start_date', 'campaigns.closing_date', 'campaigns.type', 'campaigns.category', 'campaigns.target_funding_amount', 'campaigns.status', 'users.full_name', 'user_banks.bank_name', 'user_banks.account_number')
+            ->get();
 
+        return response()->json([
+            'status' => 'success',
+            'data' => $data,
+            'server_time' => (int) round(microtime(true) * 1000),
+        ]);
+    }
 }
