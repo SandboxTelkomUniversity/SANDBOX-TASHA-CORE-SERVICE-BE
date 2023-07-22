ae396a9d41a299144836ef843400e2d55d69709d
Muhammad Rezki Ananda <muhammad.rezki.ananda@gmail.com>
2023-07-22 16:18:25 +0700

    Remove news feature in dashboard controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -27,50 +27,11 @@ class DashboardController extends Controller
         if ($authorization_level == 1) {
             $response['data']['total_asset'] = 3500000;
             $response['data']['campaigns'] = Campaign::where('status', 'ACTIVE')->limit(5)->get();
+            $response['data']['news'] = [
+                'error' => "Please redirect to news api",
+                'articles' => null,
+            ];
 
-            $apiKey = '2094e9cb485e4145b44c2b9fc7c82620';
-            $apiUrl = 'https://newsapi.org/v2/top-headlines';
-            $country = 'id';
-            $category = 'business';
-            $query = 'invest';
-            $page = 1;
-
-            $newsUrl = $apiUrl . '?apiKey=' . $apiKey . '&country=' . $country . '&category=' . $category . '&q=' . $query . '&page=' . $page;
-
-            try {
-                $curl = curl_init();
-
-                curl_setopt_array($curl, [
-                    CURLOPT_URL => $newsUrl,
-                    CURLOPT_RETURNTRANSFER => true,
-                    CURLOPT_FOLLOWLOCATION => true,
-                    CURLOPT_HTTPHEADER => [
-                        'Content-Type: application/json',
-                    ],
-                ]);
-
-                $newsData = curl_exec($curl);
-
-                if ($newsData === false) {
-                    throw new \Exception(curl_error($curl), curl_errno($curl));
-                }
-
-                curl_close($curl);
-
-                $newsData = json_decode($newsData, true);
-
-                $response['data']['news'] = [
-                    'error' => null,
-                    'articles' => ($newsData && isset($newsData['articles'])) ? $newsData['articles'] : null,
-                ];
-            } catch (\Exception $e) {
-                $errorCode = $e->getCode();
-                $errorMessage = $e->getMessage();
-                $response['data']['news'] = [
-                    'error' => "Failed to fetch news data - $errorCode - $errorMessage",
-                    'articles' => null,
-                ];
-            }
         } elseif ($authorization_level == 2) {
             $userCampaigns = Campaign::where('id_user', $user->id)->pluck('id');
 
